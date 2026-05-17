1b7456607d9948ec41abd698f5d1b6f404cc2a77
wasiqrahman2013-commits <wasiqrahman2013@gmail.com>
2026-05-17 12:17:39 +0800

    config.py
    Updated at Sunday May 17th 12:17PM 2026 GMT +8

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 DISCORD_CLIENT_ID = "1501217235589533779"
 DISCORD_CLIENT_SECRET = "SzFks7Jecc6OH6ioMsQgQGF2bIXt7rv6"
-DISCORD_REDIRECT_URI = "http://localhost:8080/auth/discord/callback"
+DISCORD_REDIRECT_URI = "https://nexelmc-website-production.up.railway.app/auth/callback"
 
 GUILD_ID = "1439964112997318709"
 
